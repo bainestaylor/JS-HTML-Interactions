deae1472448c4242b7b8e31aa1ffa51a43299e65
Taylor Baines <t.baines@share.epsb.ca>
2018-10-22 13:16:54 -0600

    new

--- scripts/childTemplate.js
+++ scripts/childTemplate.js
@@ -1,24 +1,29 @@
-console.log("You made it to childTemplate.js");
+console.log("I made it to childTemplate.js!"); //Debugging line to console ensuring JavaScript Link
 
-function main() {
-  let firstNumber, secondNumber, text;
+function main () {
 
+  let firstNumber, secondNumber; //Variables particular to JavaScript Assignment, makes it work
 
+  // Get the value of the Height Input Field, id="textfield1"
   firstNumber = document.getElementById("textfield1").value;
 
-  document.getElementById("validityTest1").innerHTML = alert ( testNaN (firstNumber) );
+  //Validate by ALERT or HTML Text in p-tag
+  document.getElementById("validityTest1").innerHTML = alert( testNaN (firstNumber) );
   document.getElementById("validityTest1").innerHTML = testNaN (firstNumber);
 
+  // Get the value of the Height Input Field, id="textfield2"
   secondNumber = document.getElementById("textfield2").value;
 
-  document.getElementById("validityTest1").innerHTML = alert ( testNaN (secondNumber) );
-  document.getElementById("validityTest1").innerHTML = testNaN (secondNumber);
+  //Validate by ALERT or HTML Text in p-tag
+  document.getElementById("validityTest2").innerHTML = alert( testNaN (secondNumber) );
+  document.getElementById("validityTest2").innerHTML = testNaN (secondNumber);
 
   if (stop == true) {
-    document.getElementById("large").innerHTML = "Restart because I said so."
+    document.getElementById("answer").innerHTML = "Restart because I said so." //Change to more appropriate message
   }
   else {
-    console.log("What did you say "+ geometry (firstNumber, secondNumber));
-    document.getElementById("large").innerHTML = "This display geometry is"
+    //Difference between calling functions with arguements and sending to parameter's, local variables
+    console.log("What did you say?", geometry (firstNumber, secondNumber)); // Change to more appropriate message
+    document.getElementById("answer").innerHTML = "What did you say? " + geometry (firstNumber, secondNumber);
   }
 }
